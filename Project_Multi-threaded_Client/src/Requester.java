5e3f44b8ecb64ff9a22cd5586edba8a79a52661c
Alexander Souza <alexpt2000@gmail.com>
2016-12-27 12:29:09 +0000

    First Commit

---
+++ Project_Multi-threaded_Client/src/Requester.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Requester {
+
+	Socket requestSocket;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	String message = "";
+	String clientResonse;
+	String ipaddress;
+	Scanner clientInput;
+
+	Requester() {
+	}
+
+	void run() {
+		clientInput = new Scanner(System.in);
+		try {
+			// 1. creating a socket to connect to the server
+			//System.out.println("Please Enter the Bank IP Address");
+			//ipaddress = stdin.next();
+
+			ipaddress = "127.0.0.1";
+
+			requestSocket = new Socket(ipaddress, 2004);
+
+			System.out.println("Connected to " + ipaddress + " in port 2004");
+
+			// 2. get Input and Output streams
+			out = new ObjectOutputStream(requestSocket.getOutputStream());
+			out.flush();
+			in = new ObjectInputStream(requestSocket.getInputStream());
+
+			try {
+				message = (String) in.readObject();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			System.out.println(message);
+
+			// 3: Communicating with the server
+			do {
+				try {
+
+					// Receive MSG from the server
+					message = (String) in.readObject();
+					System.out.println(message);
+
+					// Send MSG back
+					clientResonse = clientInput.next(); //Input type by client
+					sendMessage(clientResonse);
+
+
+				} catch (ClassNotFoundException classNot) {
+					System.err.println("data received in unknown format");
+				}
+			} while (!message.equals("exit"));
+
+		} catch (UnknownHostException unknownHost) {
+			System.err.println("You are trying to connect to an unknown host!");
+
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+
+		} finally {
+			// 4: Closing connection
+			try {
+				in.close();
+				out.close();
+				requestSocket.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
+		}
+	}
+
+	void sendMessage(String msg) {
+		try {
+			out.writeObject(msg);
+			out.flush();
+			//System.out.println("User OP.: " + msg);
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		}
+	}
+
+	public static void main(String args[]) {
+		Requester client = new Requester();
+		client.run();
+	}
+}
